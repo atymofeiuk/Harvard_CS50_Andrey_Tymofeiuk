01a33caeeea58a7d713e136ec749f8fb32b82cc3
atymofeiuk <atymofeiuk@gmail.com>
2019-06-23 13:19:56 +0300

    Harvard_CS50_PSet5_Andrey_Tymofeiuk

--- PSet5/speller (trie)/dictionary.c
+++ PSet5/speller (trie)/dictionary.c
@@ -1,3 +1,23 @@
+/**
+
+    CS50: Introduction to Computer Science (course by Harvard University, taken via edX.org)
+
+    PSet5: Speller (with "trie" data structure)
+
+    NB: PSet5/Speller file with Andrey Tymofeiuk's code is dictionary.c,
+    other additional files contain distribution code provided by Harvard CS50 staff
+
+    @author: Andrey Tymofeiuk
+
+    Important: This code is placed at GitHub to track my progress in programming and
+    to show my way of thinking. Also I will be happy if somebody finds my solution
+    interesting. But I do respect The Honor Code and I ask you to respect it also - please
+    don't submit this solution to the CS50 grader and do not copy it.
+
+    (c) 2019
+
+**/
+
 // Implements a dictionary's functionality
 
 #include <stdbool.h>
